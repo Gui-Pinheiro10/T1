2bc4ce6eadee63b22087ba6efd59a5e51be7fb01
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-10 21:51:40 -0300

    classe Cliente feita

---
+++ entidade/cliente.py
@@ -0,0 +1,16 @@
+from pessoa import Pessoa
+
+class Cliente(Pessoa):
+    def __init__(self, codigo: int, nome: str, cpf: int, rua: str, numero: int, complemento: str, idade: int):
+        super().__init__(nome, cpf, rua, numero, complemento, idade)
+        if isinstance(codigo, int):
+            self.__codigo = codigo
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, codigo: int):
+        if isinstance(codigo, int):
+            self.__codigo = codigo
