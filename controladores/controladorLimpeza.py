e9a9f2424a5a9eec219aa0912b11cce821db1b2c
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-15 20:56:06 -0300

    Update controladorLimpeza.py

--- controladores/controladorLimpeza.py
+++ controladores/controladorLimpeza.py
@@ -1,24 +1,25 @@
 from entidade.limpeza import Limpeza
 from telas.telaLimpeza import TelaLimpeza
-from controladorSistema import ControladorSistema
+#from controladores.controladorSistema import ControladorSistema
 
 
 class ControladorLimpeza:
-    def __init__(self, controlador_sistema: ControladorSistema):
-        self.__tela_limpeza = TelaLimpeza(self)
+    def __init__(self, controlador_sistema):
+        self.__tela_limpeza = TelaLimpeza()
         self.__limpezas = []
         self.__controlador_sistema = controlador_sistema
 
-    def inclui_limpeza(self):
+    def inclui_limpeza(self): # VISTO
         dados_limpeza = self.__tela_limpeza.pega_dados_limpeza()
-        limpeza = Limpeza(dados_limpeza["nome"], dados_limpeza["cpf"], dados_limpeza["idade"], dados_limpeza["rua"], dados_limpeza["numero"], dados_limpeza["complemento"], dados_limpeza["matricula"], dados_limpeza["salario"])
-        if self.pega_limpeza_por_matricula() is None:
+        limpeza = Limpeza(dados_limpeza["nome"], dados_limpeza["cpf"], dados_limpeza["idade"], dados_limpeza["rua"], dados_limpeza["numero"],
+                          dados_limpeza["complemento"], dados_limpeza["matricula"], dados_limpeza["salario"])
+        if self.pega_limpeza_por_matricula(dados_limpeza["matricula"]) is None:
             self.__limpezas.append(limpeza)
             self.__tela_limpeza.mostra_mesagem('Funcionário da Limpeza adicionado com sucesso!')
         else:
             self.__tela_limpeza.mostra_mesagem('Não foi possível cadastrar o funcionário pois a matrícula já existe!')
 
-    def exclui_limpeza(self):
+    def exclui_limpeza(self): # VISTO
         self.listar_limpezas()
         matricula_funcionario_excluido =  self.__tela_limpeza.seleciona_limpeza()
         for funcionario_limpeza in self.__limpezas:
@@ -46,12 +47,15 @@ class ControladorLimpeza:
             else:
                 self.__tela_limpeza.mostra_mesagem('Não foi possível alterar o funcionário, pois a matrícula informada não está na lista!')
 
-    def listar_limpezas(self):
-        print('Listagem de funcionários da limpeza:\n')
+    def listar_limpezas(self): # visto
+        self.__tela_limpeza.mostra_mesagem("LISTA DE FUNCIONÁRIO".center(30, '-'))
         for limpeza in self.__limpezas:
-            self.__tela_limpeza.mostra_mesagem(f'Nome: {limpeza.nome} | CPF: {limpeza.cpf} | Idade: {limpeza.idade} | Endereço: {limpeza.rua}, {limpeza.numero}, {limpeza.complemento} | Matrícula: {limpeza.matricula} ')
+            self.__tela_limpeza.mostra_mesagem({"nome": limpeza.nome, "cpf": limpeza.cpf, "idade": limpeza.idade, "rua": limpeza.endereco.rua, "numero": limpeza.endereco.numero,
+                                                "complemento": limpeza.endereco.complemento, "matricula": limpeza.matricula, "salario": limpeza.salario})
+        if len(self.__limpezas) == 0:
+            self.__tela_limpeza.mostra_mesagem("No momento a lista de funcionários de limpeza está vazio.")
 
-    def pega_limpeza_por_matricula(self, matricula: int):
+    def pega_limpeza_por_matricula(self, matricula: int): # VISTO
         for limpeza in self.__limpezas:
             if limpeza.matricula == matricula:
                 return limpeza
@@ -60,9 +64,9 @@ class ControladorLimpeza:
     def retornar(self):
         self.__controlador_sistema.abre_tela()
     
-    def abre_tela_limpeza(self):
+    def abre_tela(self):
         opcoes = {0: self.retornar, 1: self.inclui_limpeza, 2: self.altera_limpeza, 3: self.exclui_limpeza, 4: self.listar_limpezas}
-        while True:
-            opcao = self.__tela_limpeza.mostra_tela_opcoes()
-            funcao_escolhida = opcoes[opcao]
-            funcao_escolhida()
+        continua = True
+        while continua:
+            opcoes[self.__tela_limpeza.tela_opcoes()]()
+
