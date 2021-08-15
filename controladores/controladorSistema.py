6523d3fca353f15a21ad8dd141cef32551612fe4
Gui-Pinheiro10 <guilhermeifsc10@gmail.com>
2021-08-15 13:52:55 -0300

    atualização do Controlador e Tela do Sistema
    atualização do Controlador e Tela do Sistema

--- controladores/controladorSistema.py
+++ controladores/controladorSistema.py
@@ -1,9 +1,20 @@
 from telas.telaSistema import TelaSistema
 from controladores.controladorCliente import ControladorCliente
+from controladores.controladorMedico import ControladorMedico
+from controladores.controladorLimpeza import ControladorLimpeza
+from controladores.controladorEnfermeiro import ControladorEnfermeiro
+from controladores.controladorAtendente import ControladorAtendente
+from controladores.controladorAgendamento import ControladorAgendamento
+
 
 class ControladorSistema:
     def __init__(self):
         self.__controlador_cliente = ControladorCliente(self)
+        self.__controlador_medico = ControladorMedico(self)
+        self.__controlador_limpeza = ControladorLimpeza(self)
+        self.__controlador_enfermeiro = ControladorEnfermeiro(self)
+        self.__controlador_atendente = ControladorAtendente(self)
+        self.__controlador_agendamento = ControladorAgendamento(self)
         self.__tela_sistema = TelaSistema()
 
     @property
@@ -13,15 +24,30 @@ class ControladorSistema:
     def cadastro_cliente(self):
         self.__controlador_cliente.abre_tela() # chama o controlador de Clientes
 
+    def cadastro_medico(self):
+        self.__controlador_medico.abre_tela() # chama o controlador de Médicos
+
+    def cadastro_limpeza(self):
+        self.__controlador_limpeza.abre_tela() # chama o controlador de Limpeza
+
+    def cadastro_enfermeiro(self):
+        self.__controlador_enfermeiro.abre_tela() # chama o controlador de Enfermeiros
+    
+    def cadastro_atendente(self):
+        self.__controlador_atendente.abre_tela() # chama o controlador de Atendente
+
+    def cadastro_agendamento(self):
+        self.__controlador_agendamento.abre_tela() # chama o controlador de Agendamentos
+
     def inicializa_sistema(self):
         self.abre_tela()
 
-    def encarra_sistema(self):
+    def encerra_sistema(self):
         print("Sistema encerrado!")
         exit(0)
 
     def abre_tela(self):
-        lista_opcoes = {1: self.cadastro_cliente, 0: self.encarra_sistema}
+        lista_opcoes = {1: self.cadastro_cliente, 2: self.cadastro_medico, 3: self.cadastro_limpeza, 4: self.cadastro_enfermeiro, 5: self.cadastro_atendente, 6: self.cadastro_agendamento, 0: self.encerra_sistema}
 
         while True:
             opcao_escolhida = self.__tela_sistema.tela_opcoes()
--- telas/telaSistema.py
+++ telas/telaSistema.py
@@ -5,7 +5,12 @@ class TelaSistema(AbstractTela):
     def tela_opcoes(self):
         print("-------- SISTEMA CADASTRO CLIENTES ---------")
         print("Escolha sua opcao: ")
-        print("1 - CLIENTES")
+        print("1 - MENU CLIENTES")
+        print("2 - MENU MÉDICOS")
+        print("3 - MENU LIMPEZA")
+        print("4 - MENU ENFERMEIROS")
+        print("5 - MENU ATENDENTE")
+        print("6 - MENU AGENDAMENTO")
         print("0 - Finalizar sistema")
-        opcao = self.le_num_inteiro("Escolha a opcao: ", [1, 0])
+        opcao = self.le_num_inteiro("Escolha a opcao: ", [1, 2, 3, 4, 5, 6, 0])
         return opcao
