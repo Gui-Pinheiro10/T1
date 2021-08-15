9c1ebe92f88c2241fe4ee8cc6d928649c72aceb4
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-15 20:53:58 -0300

    Update controladorAgendamento.py

--- controladores/controladorAgendamento.py
+++ controladores/controladorAgendamento.py
@@ -1,11 +1,10 @@
 from entidade.agendamento import Agendamento
 from telas.telaAgendamento import TelaAgendamento
-from controladorSistema import ControladorSistema
 
 
 class ControladorAgendamento:
-    def __init__(self, controlador_sistema: ControladorSistema):
-        self.__tela_agendamento = TelaAgendamento(self)
+    def __init__(self, controlador_sistema):
+        self.__tela_agendamento = TelaAgendamento()
         self.__agendamentos = []
         self.__controlador_sistema = controlador_sistema
 
@@ -64,6 +63,6 @@ class ControladorAgendamento:
     def abre_tela_agendamento(self):
         opcoes = {0: self.retornar, 1: self.inclui_agendamento, 2: self.altera_agendamento, 3: self.exclui_agendamento, 4: self.listar_agendamentos}
         while True:
-            opcao = self.__tela_agendamento.mostra_tela_opcoes()
+            opcao = self.__tela_agendamento.tela_opcoes()
             funcao_escolhida = opcoes[opcao]
             funcao_escolhida()
