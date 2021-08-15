bb871f9f5d6572d5104f1bc558ff1457b2a74d35
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-15 20:58:02 -0300

    Update controladorSistema.py

--- controladores/controladorSistema.py
+++ controladores/controladorSistema.py
@@ -1,19 +1,19 @@
 from telas.telaSistema import TelaSistema
 from controladores.controladorCliente import ControladorCliente
-from controladores.controladorMedico import ControladorMedico
+#from controladores.controladorMedico import ControladorMedico # ainda preciso criar
 from controladores.controladorLimpeza import ControladorLimpeza
 from controladores.controladorEnfermeiro import ControladorEnfermeiro
-from controladores.controladorAtendente import ControladorAtendente
+#from controladores.controladorAtendente import ControladorAtendente # ainda preciso criar
 from controladores.controladorAgendamento import ControladorAgendamento
 
 
 class ControladorSistema:
     def __init__(self):
         self.__controlador_cliente = ControladorCliente(self)
-        self.__controlador_medico = ControladorMedico(self)
+       # self.__controlador_medico = ControladorMedico(self)
         self.__controlador_limpeza = ControladorLimpeza(self)
         self.__controlador_enfermeiro = ControladorEnfermeiro(self)
-        self.__controlador_atendente = ControladorAtendente(self)
+       # self.__controlador_atendente = ControladorAtendente(self)
         self.__controlador_agendamento = ControladorAgendamento(self)
         self.__tela_sistema = TelaSistema()
 
@@ -24,20 +24,20 @@ class ControladorSistema:
     def cadastro_cliente(self):
         self.__controlador_cliente.abre_tela() # chama o controlador de Clientes
 
-    def cadastro_medico(self):
-        self.__controlador_medico.abre_tela() # chama o controlador de Médicos
+   # def cadastro_medico(self):
+    #    self.__controlador_medico.abre_tela() # chama o controlador de Médicos
 
     def cadastro_limpeza(self):
         self.__controlador_limpeza.abre_tela() # chama o controlador de Limpeza
 
     def cadastro_enfermeiro(self):
-        self.__controlador_enfermeiro.abre_tela() # chama o controlador de Enfermeiros
+        self.__controlador_enfermeiro.abre_tela_enfermeiro() # chama o controlador de Enfermeiros
     
-    def cadastro_atendente(self):
-        self.__controlador_atendente.abre_tela() # chama o controlador de Atendente
+   # def cadastro_atendente(self):
+    #    self.__controlador_atendente.abre_tela() # chama o controlador de Atendente
 
     def cadastro_agendamento(self):
-        self.__controlador_agendamento.abre_tela() # chama o controlador de Agendamentos
+        self.__controlador_agendamento.abre_tela_agendamento() # chama o controlador de Agendamentos
 
     def inicializa_sistema(self):
         self.abre_tela()
@@ -47,8 +47,11 @@ class ControladorSistema:
         exit(0)
 
     def abre_tela(self):
-        lista_opcoes = {1: self.cadastro_cliente, 2: self.cadastro_medico, 3: self.cadastro_limpeza, 4: self.cadastro_enfermeiro, 5: self.cadastro_atendente, 6: self.cadastro_agendamento, 0: self.encerra_sistema}
+        lista_opcoes = {1: self.cadastro_cliente, 3: self.cadastro_limpeza, 4: self.cadastro_enfermeiro, 6: self.cadastro_agendamento, 0: self.encerra_sistema}
         while True:
             opcao_escolhida = self.__tela_sistema.tela_opcoes()
             funcao_escolhida = lista_opcoes[opcao_escolhida]
             funcao_escolhida()
+            # adicionar no lista opcoes
+            # 2: self.cadastro_medico
+            # 5: self.cadastro_atendente
