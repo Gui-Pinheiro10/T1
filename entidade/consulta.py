8ff2f00a360e855f321c6b2cfd27235715745db4
Gui-Pinheiro10 <guilhermeifsc10@gmail.com>
2021-08-16 10:16:20 -0300

    implementação da classe Consulta

---
+++ entidade/consulta.py
@@ -0,0 +1,23 @@
+from entidade.medico import Medico
+
+
+class Vacina():
+    def __init__(self, especialidade: str, medico: Medico):
+        self.__especialidade = especialidade
+        self.__medico = medico
+    
+    @property
+    def especialidade(self):
+        return self.__especialidade
+
+    @especialidade.setter
+    def especialidade(self, especialidade: str):
+        self.__especialidade = especialidade
+    
+    @property
+    def medico(self):
+        return self.__medico
+
+    @medico.setter
+    def medico(self, medico: Medico):
+        self.__medico = medico
