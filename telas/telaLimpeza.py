042470b2f6197927384d765743bf1af5e78fcda7
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-15 21:07:31 -0300

    Update telaLimpeza.py

--- telas/telaLimpeza.py
+++ telas/telaLimpeza.py
@@ -1,12 +1,9 @@
-from controladores.controladorLimpeza import ControladorLimpeza
 from telas.abstractTela import AbstractTela
 
 
 class TelaLimpeza(AbstractTela):
-    def __init__(self, controlador: ControladorLimpeza):
-        self.__controlador = controlador
 
-    def mostra_tela_opcoes(self):
+    def tela_opcoes(self):
         print("---------- OPÇÕES DE FUNCIONÁRIOS DA LIMPEZA ----------")
         print('1 - Incluir Funcionário da Limpeza')
         print('2 - Alterar Funcionário da Limpeza')
@@ -14,34 +11,43 @@ class TelaLimpeza(AbstractTela):
         print('4 - Listar os Funcionários da Limpeza')
         print('0 - Retornar')
         opcao = self.le_num_inteiro('Escolha a opção: ', [0, 1, 2, 3, 4])
+        print('\n')
         return opcao
 
     def pega_dados_limpeza(self):
         print('---------- DADOS DO FUNCIONÁRIO DA LIMPEZA ----------')
         nome = input('Nome: ')
         cpf = input('CPF: ')
-        idade = input('Idade: ')
+        idade = int(input('Idade: '))
         print('Digite os dados do seu endereço abaixo:')
         rua = input('Rua: ')
-        numero = input('Número: ')
+        numero = int(input('Número: '))
         complemento = input('Complemento: ')
-        matricula = input('Matrícula: ')
-        salario = input('Salário:')
+        matricula = int(input('Matrícula: '))
+        salario = float(input('Salário:'))
         return {"nome": nome, "cpf": cpf, "idade": idade, "rua": rua, "numero": numero, "complemento": complemento,
                 "matricula": matricula, "salario": salario}
 
     def pega_dados_para_alterar_limpeza(self):
         print('---------- DADOS PARA ALTERAÇÃO DE FUNCIONÁRIO DA LIMPEZA ----------')
-        print('ATENÇÃO! Não é permitido alterar CPF e matrícula de enfermeiro.')
+        print('ATENÇÃO! Não é permitido alterar CPF e matrícula de funcionário de limpeza.')
         nome = input('Nome: ')
-        idade = input('Idade: ')
+        idade = int(input('Idade: '))
         salario = input('Salário: ')
         print('Digite os novos dados do seu endereço abaixo:')
         rua = input('Rua: ')
-        numero = input('Número: ')
+        numero = int(input('Número: '))
         complemento = input('Complemento: ')
         return {"nome": nome, "idade": idade, "rua": rua, "numero": numero, "complemento": complemento, "salario": salario}
-        
+
+    def mostra_funcionario_limpeza(self, dados_funcionario):
+        print("NOME: ", dados_funcionario["nome"])
+        print("CPF: ",dados_funcionario["cpf"])
+        print("IDADE: ", dados_funcionario["idade"])
+        print("SALÁRIO: ", dados_funcionario["salario"])
+        print("MATRÍCULA: ", dados_funcionario["matricula"])
+        print("ENDEREÇO: Rua: ",dados_funcionario["rua"], "// Número: ",dados_funcionario["numero"], "// Complemento: ", dados_funcionario["complemento"])
+
     def seleciona_limpeza(self):
-        matricula = input('Digite a matrícula do funcionário da limpeza que deseja selecionar: ')
+        matricula = int(input('Digite a matrícula do funcionário da limpeza que deseja selecionar: '))
         return matricula
