488472a5bc92854355da5ea93a08053e4e651d42
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-16 08:55:25 -0300

    Update medico.py

---
+++ entidade/medico.py
@@ -0,0 +1,18 @@
+from entidade.funcionario import Funcionario
+
+class Medico(Funcionario):
+    def __init__(self, nome: str, cpf: str, idade: int, rua: str, numero: int, complemento: str,
+                 matricula: int, salario: float, crm: str):
+        super().__init__(nome, cpf, idade, rua, numero, complemento, matricula, salario)
+        if isinstance(crm, str):
+            self.__crm = crm
+
+    @property
+    def crm(self):
+        return self.__crm
+
+    @crm.setter
+    def crm(self, crm: str):
+        if isinstance(crm, str):
+            self.__crm = crm
+            
