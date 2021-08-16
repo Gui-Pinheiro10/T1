26703a78101e5f37d39b8a2adc92c45709b3dd23
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-15 21:01:18 -0300

    Update enfermeiro.py

--- entidade/enfermeiro.py
+++ entidade/enfermeiro.py
@@ -1,6 +1,6 @@
-from funcionario import Funcionario
+from entidade.funcionario import Funcionario
 
 
 class Enfermeiro(Funcionario):
-    def __init__(self, nome, cpf, idade, rua, numero, complemento, matricula, salario):
+    def __init__(self, nome: str, cpf: str, idade: int, rua: str, numero: int, complemento: str, matricula: int, salario: float):
         super().__init__(nome, cpf, idade, rua, numero, complemento, matricula, salario)
