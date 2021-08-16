a1a63bf472c5f874ddd034754f50c6ea7b8e4edb
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-08-16 14:26:31 -0300

    arrumei init classe Atendente

--- entidade/atendente.py
+++ entidade/atendente.py
@@ -2,6 +2,6 @@ from entidade.funcionario import Funcionario
 
 
 class Atendente(Funcionario):
-    def __init__(self, nome: str, idade: int, cpf: str, rua: str, numero: int, complemento: str,
+    def __init__(self, nome: str, cpf: str, idade: int, rua: str, numero: int, complemento: str,
                  matricula: int, salario: float):
         super().__init__(nome, cpf, idade, rua, numero, complemento, matricula, salario)
