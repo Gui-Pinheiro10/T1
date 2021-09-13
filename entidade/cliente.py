37f24b3801bd938f1d0713f2f6f2137df624a983
marinaemerim <87453159+marinaemerim@users.noreply.github.com>
2021-09-13 15:06:35 -0300

    retirei o atributo "codigo" pq não vou mais usar

--- entidade/cliente.py
+++ entidade/cliente.py
@@ -1,16 +1,5 @@
 from entidade.pessoa import Pessoa
 
 class Cliente(Pessoa):
-    def __init__(self, nome: str, cpf: str, idade: int, rua: str, numero: int, complemento: str,  codigo: int):
+    def __init__(self, nome: str, cpf: str, idade: int, rua: str, numero: int, complemento: str):
         super().__init__(nome, cpf, idade, rua, numero, complemento)
-        if isinstance(codigo, int):
-            self.__codigo = codigo
-
-    @property
-    def codigo(self):
-        return self.__codigo
-
-    @codigo.setter
-    def codigo(self, codigo: int):
-        if isinstance(codigo, int):
-            self.__codigo = codigo
